d9c56455eb0986be6426dc1d3a8fe75f5518a264
Phil <phil@hengetech.com.tw>
2024-05-14 16:17:43 +0800

    add

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,3 +2,5 @@
 if [ -f "./.devcontainer/welcome-message.txt" ]; then
   sudo cp --force ./.devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 fi
+
+echo "hello"
